88d106ce35d09245217e6f149903e34ca78e71d4
Marta <mbernapo49@alumnes.ub.edu>
2024-05-20 11:54:48 +0200

    pedroooo

--- tmproject/__main__.py
+++ tmproject/__main__.py
@@ -1,10 +1,12 @@
 import threading
 import time
+from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
 
 import click
 import cv2
+import imageio
 import numpy as np
 from PIL import Image
 
@@ -27,7 +29,6 @@ CONV_FILTERS_DESCRIPTION = {
     'laplacian': 'Filtre convolucional Laplacian per a ressaltar detalls i detectar contorns utilitzant la mida del '
                  'kernel indicada.',
 }
-
 
 # Mètode per aplicar els filtres puntuals a la imatge
 def apply_filters(img_array, filters):
@@ -131,38 +132,62 @@ def play_images(images_dir, fps):
         Returns:
         None
     """
-    # Obté les imatges en JPEG
-    image_paths = sorted(images_dir.glob('*.jpg'))
     while True:
         interval = 1.0 / fps
-        for image_path in image_paths:
+        for image_name in images_dir:
             # Llegeix la imatge i la mostra
-            img = cv2.imread(str(image_path))
-            cv2.imshow('Image', img)
+            cv2.imshow('Image', image_name)
             # Espera a prèmer la tecla 'q' per parar el vídeo
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                # esborrar la carpeta d'imatges temporals
-                for file_path in images_dir.iterdir():
-                    file_path.unlink()
-                images_dir.rmdir()
                 return
             # Espera abans de mostrar la següent imatge
             time.sleep(interval)
 
+# Funció per llegir imatges contingudes en un ZIP
+def read_images_from_zip(zip_file_path):
+    images = []
+    with ZipFile(zip_file_path, 'r') as zip_ref:
+        for file_info in zip_ref.infolist():
+            with zip_ref.open(file_info) as file:
+                # Llegir imatge com a buffer de bytes
+                image_bytes = file.read()
+                # Convertir els bytes a imatge utilitzant Pillow
+                image = Image.open(BytesIO(image_bytes))
+                images.append(image)
+    return images
 
+# Funció per llegir imatges contingudes en un GIF
+def read_images_from_gif(gif_file_path):
+    gif = cv2.VideoCapture(gif_file_path)
+    images = []
+    while True:
+        ret, frame = gif.read()
+        if not ret:
+            break
+        images.append(frame)
+    gif.release()
+    return images
+
+# Funció per llegir frames d'un vídeo
+def read_frames_from_video(video_file_path):
+    frames = []
+    cap = cv2.VideoCapture(video_file_path)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        # Convertir frame a RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames.append(frame_rgb)
+    cap.release()
+    return frames
 @click.command()
 @click.option('-i', '--input', required=True, type=click.Path(exists=True), help='Fitxer d’entrada. Argument '
                                                                                  'obligatori.')
 @click.option('-o', '--output', type=click.Path(),
               help='Nom del fitxer en format propi amb la seqüència d’imatges de sortida i la informació necessària '
                    'per la descodificació.')
-@click.option('-e', '--encode', is_flag=True,
-              help='Argument que indica que s’haurà d’aplicar la codificació sobre el conjunt d’imatges d’input i '
-                   'guardar el resultat al lloc indicat per output. En acabar, s’ha de procedir a reproduir el '
-                   'conjunt d’imatges sense codificar (input).')
-@click.option('-d', '--decode', is_flag=True,
-              help='Argument que indica que s’haurà d’aplicar la descodificació sobre el conjunt d’imatges d’input '
-                   'provinents d’un fitxer en format propi i reproduir el conjunt d’imatges descodificat (output). ')
+
 @click.option('--fps', default=25, type=int, help='nombre d’imatges per segon amb les quals és reproduirà el vídeo.')
 @click.option('--filters', help='''Lista de filtros puntuales separados por comas.
 {}'''.format('\n'.join([f"{key}: {val}\n" for key, val in FILTERS_DESCRIPTION.items()])))
@@ -175,58 +200,59 @@ def play_images(images_dir, fps):
 @click.option('--GOP', type=int, help='nombre d’imatges entre dos frames de referència.')
 @click.option('--quality', type=float,
               help='factor de qualitat que determinarà quan dos tessel·les és consideren coincidents.')
-@click.option('-b', '--batch', is_flag=True,
-              help='en aquest mode no s’obrirà cap finestra del reproductor de vídeo. Ha de permetre executar el '
-                   'còdec a través de Shell scripts per avaluar de forma automatitzada el rendiment de l’algorisme '
-                   'implementat en funció dels diferents paràmetres de configuració.')
-def main(input, output, encode, decode, fps, filters, conv_filters, ntiles, seekrange, gop, quality, batch):
+
+def main(input, output, fps, filters, conv_filters, ntiles, seekrange, gop, quality):
     filter_dict = {}
     conv_filter_dict = {}
+
     # Processament dels filtres puntuals
     if filters:
         filter_dict = {
             filter_item.split('[')[0]: filter_item.split('[')[1].split(']')[0] if '[' in filter_item else None
             for filter_item in filters.split(',')
         }
+
     # Processament dels filtres convolucionals
     if conv_filters:
         conv_filter_dict = {
             filter_item.split('[')[0]: filter_item.split('[')[1].split(']')[0] if '[' in filter_item else None
             for filter_item in conv_filters.split(',')
         }
-    # Extreure imatges del ZIP
-    with ZipFile(input, 'r') as zip_ref:
-        zip_ref.extractall('temp_images')
-    images_dir = Path('temp_images')
-    # Llegeix les imatges de dintre el zip que estan en format JPEG, PNG BMP i GIF
-    for file_path in images_dir.iterdir():
-        if file_path.suffix in ['.jpg', '.png', '.bmp', '.gif']:
-            img = Image.open(file_path)
-            img_array = np.array(img)
-            # Aplica els filtres puntuals que s'han especificat
-            if filters:
-                img_array = apply_filters(img_array, filter_dict)
-            # Aplica els filtres convolucionals que s'han especificat
-            if conv_filters:
-                img_array = apply_conv_filters(img_array, conv_filter_dict)
-            # Guarda les imatges amb els filtres aplicats
-            img_array = Image.fromarray(img_array)
-            img_array.save(file_path)
 
-    # Guarda les imatges en format JPEG
-    for file_path in images_dir.iterdir():
-        if file_path.suffix in ['.png', '.bmp', '.gif']:
-            img = Image.open(file_path)
-            img.save(images_dir.joinpath(file_path.stem + '.jpg'))
-    # Si s'indica l'opció output, guarda les imatges en un ZIP
+    if input.endswith('.zip'):
+        images = read_images_from_zip(input)
+    elif input.endswith('.gif'):
+        images = read_images_from_gif(input)
+    elif input.endswith(('.avi', '.mpeg', '.mp4')):
+        images = read_frames_from_video(input)
+    else:
+        print("Format d'arxiu no suportat. Si us plau, proporciona un arxiu en format ZIP, GIF o vídeo.")
+        return
+
+    for i, img in enumerate(images):
+        img_array = np.array(img)
+        # Aplica els filtres puntuals que s'han especificat
+        if filters:
+            img_array = apply_filters(img_array, filter_dict)
+        # Aplica els filtres convolucionals que s'han especificat
+        if conv_filters:
+            img_array = apply_conv_filters(img_array, conv_filter_dict)
+            # Converteix l'array de numpy en imatge
+        img = Image.fromarray(img_array)
+        # Desa la imatge com a JPEG
+        img.save(f'image_{i}.jpeg')
+
+    # Si s'ha especificat la opció output, guardem les imatges en un arxiu ZIP
     if output:
         with ZipFile(output, 'w') as new_zip:
-            for file_path in images_dir.iterdir():
-                new_zip.write(file_path, file_path.name)
+            for i in range(len(images)):
+                new_zip.write(f'image_{i}.jpeg')
 
-    play_thread = threading.Thread(target=play_images, args=(images_dir, fps))
+    # Inicia la reproducció de les imatges en un thread separat
+    play_thread = threading.Thread(target=play_images, args=(images, fps))
     play_thread.start()
     play_thread.join()
+
 
 
 if __name__ == '__main__':
